61cfbf634d1735a042d5f9905452e764932f62db
AndreasHolmAndersen <andreasholmandersen@macbook-pro-tilhrende-andreas.home>
2021-11-29 11:10:02 +0100

    created backend for getting tours

---
+++ src/main/java/com/example/ahlbergbackend/AhlbergBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ahlbergbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhlbergBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhlbergBackendApplication.class, args);
+	}
+
+}
