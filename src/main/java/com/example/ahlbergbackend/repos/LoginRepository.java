d1e9f3680d21eccc90d0b4902f05b9212f550213
jonasegehrke <jonasegehrke@gmail.com>
2021-12-08 10:20:27 +0100

    login service & REST

---
+++ src/main/java/com/example/ahlbergbackend/repos/LoginRepository.java
@@ -0,0 +1,12 @@
+package com.example.ahlbergbackend.repos;
+
+import com.example.ahlbergbackend.model.Login;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginRepository extends JpaRepository<Login, Integer> {
+
+
+
+}
